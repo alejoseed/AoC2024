61e0c9c274742a61ce32eb1cfcb2be45aa4ea6fa
Alejandro Palmar <alejandrojpalmar@hotmail.com>
2024-12-08 23:11:51 -0500

    attempt at day 5

--- day5/aoc_5.1.cpp
+++ day5/aoc_5.1.cpp
@@ -4,9 +4,16 @@
 #include <tuple>
 #include <string>
 #include <bits/stdc++.h>
+#include <unordered_map>
 
 using namespace std;
 void createAdjList();
+
+unordered_map<int, vector<int>> adj_list;
+vector<vector<int>> verification;
+
+using namespace std;
+void getInputs();
 
 unordered_map<int, vector<int>> adj_list;
 vector<vector<int>> verification;
@@ -15,44 +22,69 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    string line;
-    
-    bool firstPart = true;
-    while (getline(cin, line)) {
-        string num;
-        vector<int> curLine;
+    getInputs();
 
-        for (const char &ch : line) {
-            num += ch;
-            if (ch == '|' && firstPart) {
-                if (adj_list.contains(stoi(num))) {
-                    continue;
+    for (const auto& line : verification) {
+        bool exists = false;
+        for (int i = 0; i < line.size(); i++) {
+            bool curNumExists = false;
+            int key = line[i];
+            for (const auto& val : adj_list[key]) {
+                for (int j = i + 1; j < line.size(); j++) {
+                    if (line[j] == val) curNumExists = true;
                 }
-                adj_list.insert_or_assign(stoi(num), vector<int>{});
-                num.clear();
-            }
-            else if (ch == ',' && firstPart == false) {
-                curLine.push_back(stoi(num));
-                num.clear();
-            }
-            else if (ch == '\n') {
-                firstPart = false;
+                if (curNumExists == false) exists = false;
+                else exists = true;
             }
         }
+        if (exists == true) cout << "true line" << "\n";
+    }
+    return 0;
+}
 
-        if (firstPart == false) {
+void getInputs() {
+    string line;
+    bool firstPart = true;
+
+    while (getline(cin, line)) {
+        if (line.size() == 0) {
+            firstPart = false;
+            continue;
+        };
+
+        if (firstPart) {
+            int key, val;
+            key = stoi(line.substr(0, 2));
+            val = stoi(line.substr(3, line.size() - 1));
+
+            if (adj_list.contains(key)) {
+                adj_list[key].push_back(val);
+                if (!adj_list.contains(val)) {
+                    vector<int> temp;
+                    adj_list.insert_or_assign(val, temp);
+                }
+            }
+            else {
+                vector<int> curLine;
+                curLine.push_back(val);
+                adj_list.insert_or_assign(key, curLine);
+            }
+        }
+        else {
+            string num;
+            vector<int> curLine;
+            for (const char& ch : line) {
+                if (ch == ',' && num.size() == 2) {
+                    curLine.push_back(stoi(num));
+                    num.clear();
+                    continue;
+                }
+                num += ch;
+            }
+            if (num.size() == 2) {
+                curLine.push_back(stoi(num));
+            }
             verification.push_back(curLine);
         }
     }
-    
-    for (const auto &[key, line] : adj_list) {
-        cout << "Key:" << " " << key;
-    }
-
-    return 0;
-
-}
-
-void createAdjList() {
-    return;
 }
